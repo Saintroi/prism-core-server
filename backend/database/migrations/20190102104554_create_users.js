6b197f64e9a73464c6f17f1a5525131870097bf9
Drew Nelson <talonroi21@gmail.com>
2019-01-18 10:19:30 -0600

    Added client-side express server

--- backend/database/migrations/20190102104554_create_users.js
+++ backend/database/migrations/20190102104554_create_users.js
@@ -17,3 +17,6 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable('user');
 };
+
+// EXAMPLE INSERT: 
+// insert into user (first_name, last_name, email, type, cell_phone, title, location, admin) VALUES ('Drew', 'Nelson', 'drew.nelson@prismsystems.com', 'employee', '(205) 908-8720', 'Systems Design Group', 'Birmingham', true);
